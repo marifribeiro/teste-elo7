066b8d01905e5df1854c97b5255dbb2d5f3875ae
Mariana Fernandes Ribeiro <mariana.ribeiro@pravaler.com.br>
2021-08-12 09:19:59 -0300

    separating types in another file

--- src/components/Hiperlink/Hiperlink.tsx
+++ src/components/Hiperlink/Hiperlink.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 
+import { HiperlinkProps } from "./Hiperlink.types";
 import styles from "./Hiperlink.module.scss";
 
-type Props = {
-  label: string;
-  url: string;
-};
-
-export const Hiperlink = ({ label, url }: Props) => {
+export const Hiperlink = ({ label, url }: HiperlinkProps) => {
   return (
     <div className={styles.hiperlink}>
       <span></span>
---
+++ src/components/Hiperlink/Hiperlink.types.ts
@@ -0,0 +1,4 @@
+export interface HiperlinkProps {
+  label: string;
+  url: string;
+}
