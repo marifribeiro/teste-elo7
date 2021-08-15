ca3ac030bc0e38480a8c2670854e625cec0a2a6c
Mariana Fernandes Ribeiro <mariana.ribeiro@pravaler.com.br>
2021-08-15 12:48:49 -0300

    changins jest coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   testEnvironment: "jsdom",
   testPathIgnorePatterns: ["/node_modules/"],
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.ts(x)?"],
+  collectCoverageFrom: [
+    "src/**/*.ts(x)?",
+    "src/**/{!(index),}.ts",
+    "src/**/*.{!(types),}.ts"
+  ],
   moduleDirectories: ["node_modules", "src"]
 };
